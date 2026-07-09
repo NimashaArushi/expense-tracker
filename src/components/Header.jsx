760a5a517e8eabe091dc87a302b7a3c220157417
arushi <arushinimasha@gmail.com>
2026-07-09 17:38:34 +0530

    1.1 header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h2>💰 Expense Tracker</h2>
+      <button className="reset-btn">Reset All</button>
+    </header>
+  );
+};
+
+export default Header;
