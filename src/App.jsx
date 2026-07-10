daa53fcd82429a61bec3846c33d570871f8048e5
arushi <arushinimasha@gmail.com>
2026-07-10 10:05:29 +0530

    1.4 TransactionList components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import TransactionForm from './components/TransactionForm';
+import TransactionList from './components/TransactionList';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Balance />
       <TransactionForm />
+      <TransactionList />
     </div>
   );
 }
---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+const TransactionList=()=>{
+    return(
+   <div className="list-container">
+    <h3>History</h3>
+    <ul className="List">
+        <li className="plus">
+            <span>Salary  </span>
+            <span>  50,000 LKR     </span>
+            <button className="delete-btn">X</button>
+        </li>
+   <li className="minus">
+          <span> Buy Shoes  </span>
+          <span> -4,500 LKR     </span>
+          <button className="delete-btn"> x </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default TransactionList;
