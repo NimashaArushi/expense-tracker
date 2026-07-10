460c224c5107d188846507f032a9134063c86ddc
arushi <arushinimasha@gmail.com>
2026-07-10 17:39:16 +0530

    3.3: Make TransactionForm a controlled component and handle submit locally

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,24 +1,61 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const TransactionForm = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+  const [type, setType] = useState('income');
+
+  const onSubmit=(e)=>{
+    e.preventDefault();
+  
+  if(text.trim()==='' || amount.trim()===''){
+    alert('plz Enter the descritions and amount !!!');
+    return;
+  }
+
+  const newTransaction={
+    id:Math.floor(Math.random()*10000000),
+    text:text,
+    amount:+amount,
+    type:type
+  
+  }
+
+
+  console.log(newTransaction);
+  setText('');
+    setAmount('');
+  };
   return (
     <div className="form-container">
       <h3>Add New Transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
-         
-          <label htmlFor="text">Description </label>
-          <input type="text" placeholder="Enter description..." />
-        </div>
+         <label htmlFor="text">Description </label>
+          <input
+          type="text"
+          value={text}
+          onChange={(e)=>setText(e.target.value)}
+          placeholder="Enter description..."/>
+</div>
+
+
         <div className="form-control">
           <label htmlFor="amount">Amount </label>
-          <input type="number" placeholder="Enter amount..." />
+      <input 
+            type="number" 
+            value={amount} 
+            onChange={(e) => setAmount(e.target.value)} 
+            placeholder="Enter amount..." 
+          />
         </div>
+
+
         <div className="form-control">
           <label htmlFor="type">Type </label>
-          <select>
-            <option value="income">Income (+)</option>
-            <option value="expense">Expense (-)</option>
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="income">Income (ආදායම)</option>
+            <option value="expense">Expense (වියදම)</option>
           </select>
         </div>
         <button className="btn">Add Transaction</button>
