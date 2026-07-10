085cb10742131bc3a48516210f8abe7854fdd3fa
arushi <arushinimasha@gmail.com>
2026-07-10 10:25:38 +0530

    2.0 Add full CSS styling and fix layout issues

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -3,15 +3,15 @@ const TransactionList=()=>{
     return(
    <div className="list-container">
     <h3>History</h3>
-    <ul className="List">
+    <ul className="list">
         <li className="plus">
-            <span>Salary  </span>
-            <span>  50,000 LKR     </span>
+            <span>Salary</span>
+            <span> 50,000 LKR</span>
             <button className="delete-btn">X</button>
         </li>
    <li className="minus">
-          <span> Buy Shoes  </span>
-          <span> -4,500 LKR     </span>
+          <span>Buy Shoes</span>
+          <span>-4,500 LKR</span>
           <button className="delete-btn"> x </button>
         </li>
       </ul>
